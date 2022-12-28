31d7813f4a68e81f5d1dcdfc2bac9fad4c849036
Ilya <ilya.tkachov@hotmail.com>
2022-12-27 21:22:57 -0500

    fix

--- modules/components/Badge/Badge.tsx
+++ modules/components/Badge/Badge.tsx
@@ -8,7 +8,7 @@ export default function Badge(props: Props): JSX.Element {
   const { children } = props
 
   return (
-    <span className="inline-flex items-center m-1 px-2 py-1 bg-indigo-100  rounded-full text-sm font-semibold text-indigo-500">
+    <span className="inline-block align-middle items-center m-1 px-2 py-1 bg-indigo-100  rounded-full text-sm font-semibold text-indigo-500">
       {children}
     </span>
   )
