03805b073a05108f994fdae63612205b02508555
Ilya <ilya.tkachov@hotmail.com>
2022-12-28 15:43:36 -0500

    fix

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,16 +1,16 @@
-import ExternalLink from "@/modules/components/ExternalLink/ExternalLink"
-import { GITHUB } from "@/modules/constants"
+import ExternalLink from '@/modules/components/ExternalLink/ExternalLink'
+import { GITHUB } from '@/modules/constants'
 
 export default function Home() {
   return (
     <>
-      <div className="flex fixed items-center justify-center left-0 top-0">
+      <div className='flex fixed items-center justify-center left-0 top-0'>
         <ExternalLink href={GITHUB}>
-          <p className="p-1 text-xs text-black">github</p>
+          <p className='p-1 text-xs text-black'>github</p>
         </ExternalLink>
       </div>
-      <div className="flex pointer-events-none fixed items-center justify-center left-0 bottom-0 ">
-        <p className="p-1 text-xs text-black">i9v.com</p>
+      <div className='flex pointer-events-none fixed items-center justify-center left-0 bottom-0 '>
+        <p className='p-1 text-xs text-black'>www.ilyatkachov.com</p>
       </div>
     </>
   )
